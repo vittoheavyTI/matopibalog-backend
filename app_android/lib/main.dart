7f32e75513c081dfd3aab042eba3841fefe9e714
Vittor <vittoheavymetal@gmail.com>
2026-06-23 16:53:28 -0300

    fix(app): importar services para orientacao vertical

--- app_android/lib/main.dart
+++ app_android/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:workmanager/workmanager.dart';
 import 'providers/auth_provider.dart';
