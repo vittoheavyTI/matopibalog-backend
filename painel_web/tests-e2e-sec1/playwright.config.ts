d1239f9a8e962acc965369b854cc4ded0e43a840
Vittor <vittoheavymetal@gmail.com>
2026-08-08 18:21:49 -0300

    fix(sec-1): habilita third-party cookies no modo e2e permitido

--- painel_web/tests-e2e-sec1/playwright.config.ts
+++ painel_web/tests-e2e-sec1/playwright.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     launchOptions: {
       args: [
         '--host-resolver-rules=MAP app.sec1.test 127.0.0.1,MAP api.sec1.invalid 127.0.0.1,MAP evil.sec1.test 127.0.0.1',
+        '--disable-features=BlockThirdPartyCookies,ThirdPartyStoragePartitioning,ThirdPartyCookiePhaseout,TrackingProtection3pcd',
       ],
     },
   },
